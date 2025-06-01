8460e56f8ce55fb4023f0c0f3b33bbe2d42eb4d9
Shriyans Sudhi <shriyanss@ss0x00.com>
2025-06-01 21:55:29 +0530

    Add feature to download lazy loaded files in Next.js apps

---
+++ globalConfig.js
@@ -0,0 +1,8 @@
+const githubURL = "https://github.com/shriyanss/js-recon"
+
+global.CONFIG = {
+    github: githubURL,
+    notFoundMessage: `If you believe this is an error or is a new technology, please create an issue on ${githubURL} and we'll figure it out for you`,
+};
+
+export default CONFIG;
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ program
   .command("lazyload")
   .description("Run lazy load module")
   .requiredOption("-u, --url <url>", "Target URL")
-  .option("-o, --output <directory>", "Output directory")
+  .option("-o, --output <directory>", "Output directory", "output")
   .action(async (cmd) => {
     await lazyload(cmd.url, cmd.output);
   });
