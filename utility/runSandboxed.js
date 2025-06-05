a2ca41bdb02a1600a3818910e6f4e33effcf3acf
Shriyans Sudhi <shriyanss@ss0x00.com>
2025-06-05 20:31:59 +0530

    Replace 'vm2' with 'ses' as it was unsecure

--- utility/runSandboxed.js
+++ utility/runSandboxed.js
@@ -1,13 +1,25 @@
-import { VM } from "vm2";
+import 'ses';
 
+// Lock down the global environment for security
+lockdown();
+
+/**
+ * Executes a given function in a sandboxed environment.
+ * @param {string} code - The code to execute.
+ * @param {any} param - The parameter to pass to the function.
+ * @returns {any} The result of the function execution.
+ */
 const execFunc = (code, param) => {
-    const vm = new VM({
-        timeout: 2000,
-        sandbox: {},
-    });
-    const func = vm.run(code);
-    const output = func(param);
-    return output;
+  const c = new Compartment({
+    console,
+  });
+  const wrappedCode = `
+    (${code})
+  `;
+  const func = c.evaluate(wrappedCode);
+  const output = func(param);
+
+  return output;
 };
 
 export default execFunc;
