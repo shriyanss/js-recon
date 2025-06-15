c7643852561783148381f453319751051725aef8
Shriyans Sudhi <shriyanss@ss0x00.com>
2025-06-15 23:01:43 +0530

    feat: add firewall detection and feasibility check for API Gateway requests

---
+++ api_gateway/checkFireWallBlocking.js
@@ -0,0 +1,14 @@
+import chalk from "chalk";
+
+const checkFireWallBlocking = async (body) => {
+
+    // check common signs of CF first
+    if (body.includes("<title>Just a moment...</title>")) {
+        console.log(chalk.red("[!] Cloudflare detected"));
+        return true;
+    }
+    
+    return false;
+};
+
+export default checkFireWallBlocking;
