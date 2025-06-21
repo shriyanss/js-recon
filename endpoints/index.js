38a57e0e8f18de26dac183cc7c22953a43715120
Shriyans Sudhi <shriyanss@ss0x00.com>
2025-06-21 13:39:52 +0530

    feat: add technology-specific endpoint extraction with OpenAPI output support (just the CLI code structure, and not the feature)

--- endpoints/index.js
+++ endpoints/index.js
@@ -1,7 +1,41 @@
 import chalk from "chalk";
 
-const endpoints = () => {
-    console.log(chalk.green("Feature under development. Check back later :)"));
+const techs = [
+    "Next.JS (next)",
+    "Nuxt.JS (nuxt)",
+    "Svelte (svelte)"
+]
+
+const endpoints = async (directory, output, tech, list) => {
+    console.log(chalk.cyan("[i] Loading endpoints module"));
+    
+    if (list) {
+        console.log(chalk.cyan("[i] Listing available technologies"));
+        for (const tech of techs) {
+            console.log(chalk.greenBright(`- ${tech}`));
+        }
+        return;
+    }
+
+    if (!directory) {
+        console.log(chalk.red("[!] Please provide a directory"));
+        return;
+    }
+
+    if (!tech) {
+        console.log(chalk.red("[!] Please provide a technology"));
+        return;
+    }
+
+    if (!output) {
+        console.log(chalk.red("[!] Please provide an output file"));
+        return;
+    }
+
+    console.log(chalk.cyan("[i] Extracting endpoints"));
+
+    
+
 };
 
 export default endpoints;
