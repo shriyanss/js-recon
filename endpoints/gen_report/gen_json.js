83bff317529a01463cdc0e0e7aae802cc006674c
Shriyans Sudhi <shriyanss@ss0x00.com>
2025-06-23 10:31:54 +0530

    refactor: move iterate_n_store function to separate utility module

--- endpoints/gen_report/gen_json.js
+++ endpoints/gen_report/gen_json.js
@@ -1,50 +1,6 @@
 import fs from "fs";
 import chalk from "chalk";
-import resolvePath from "../../utility/resolvePath.js";
-
-const iterate_n_store = async (baseUrl, urls) => {
-  let result = {};
-  for (let url of urls) {
-    if (url.startsWith("mailto:") || url.startsWith("tel:")) {
-      continue;
-    }
-
-    if (url.startsWith("/")) {
-      url = await resolvePath(baseUrl, url);
-    }
-
-    // Parse the URL once and extract the bits we need
-    const { origin, pathname } = new URL(url);
-
-    // Ensure we have a container object for this origin
-    if (!result[origin]) {
-      result[origin] = {};
-    }
-
-    // Always insert the root path for this origin
-    if (!result[origin]["/"]) {
-      result[origin]["/"] = {};
-    }
-
-    // Normalise the pathname – strip leading/trailing slashes and split into segments
-    const segments =
-      pathname === "/" ? [] : pathname.replace(/^\/|\/$/g, "").split("/");
-
-    let cumulativePath = ""; // will build up like "/app", "/app/dashboard"
-    let currentNode = result[origin];
-
-    for (const segment of segments) {
-      cumulativePath += `/${segment}`;
-      if (!currentNode[cumulativePath]) {
-        currentNode[cumulativePath] = {};
-      }
-      // descend into the newly created / already existing child
-      currentNode = currentNode[cumulativePath];
-    }
-  }
-
-  return result;
-};
+import iterate_n_store from "./utility/iterate_n_store.js";
 
 const gen_json = async (url, hrefs, output) => {
   // iterate over hrefs
