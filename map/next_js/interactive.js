122ca15c550832f7de54418fcb64c5753d6e883c
Shriyans Sudhi <shriyanss@ss0x00.com>
2025-07-01 00:17:37 +0530

    feat: implement function navigation with history in interactive mode

--- map/next_js/interactive.js
+++ map/next_js/interactive.js
@@ -4,36 +4,48 @@ import chalk from "chalk";
 const helpMenu = {
   help: "Show this help menu",
   exit: "Exit the interactive mode (or press escape twice)",
-  fetch: "Show list of chunks that contain fetch instances",
-  func: "Show function code",
+  list: "Show list of chunks",
+  go: "Go to a specific function",
 };
 
-const fetchMenu = (chunks) => {
-  let returnText = chalk.cyan("List of chunks that contain fetch instances\n");
-  for (const chunk of Object.values(chunks)) {
-    if (chunk.containsFetch) {
-      returnText += chalk.green(`- ${chunk.id}: ${chunk.file}\n`);
+const commandHelpers = {
+  fetchMenu: (chunks) => {
+    let returnText = chalk.cyan(
+      "List of chunks that contain fetch instances\n"
+    );
+    for (const chunk of Object.values(chunks)) {
+      if (chunk.containsFetch) {
+        returnText += chalk.green(`- ${chunk.id}: ${chunk.file}\n`);
+      }
     }
-  }
-  return returnText;
-};
-
-const getFunctionCode = (chunks, funcName) => {
-  let funcCode;
-  for (const chunk of Object.values(chunks)) {
-    if (chunk.id == funcName) {
-      funcCode = chunk.code;
+    return returnText;
+  },
+  getFunctionCode: (chunks, funcName) => {
+    let funcCode;
+    for (const chunk of Object.values(chunks)) {
+      if (chunk.id == funcName) {
+        funcCode = chunk.code;
+      }
     }
-  }
-  if (!funcCode) {
-    return chalk.red(`Function ${funcName} not found`);
-  }
-  return funcCode;
+    if (!funcCode) {
+      return chalk.red(`Function ${funcName} not found`);
+    }
+    return funcCode;
+  },
+  listAllFunctions: (chunks) => {
+    let returnText = chalk.cyan("List of all functions\n");
+    for (const chunk of Object.values(chunks)) {
+      returnText += chalk.green(`- ${chunk.id}: ${chunk.description}\n`);
+    }
+    return returnText;
+  },
 };
 
 const interactive = async (chunks) => {
   // store whether the last command was successful or not
   let lastCommandStatus = true;
+  let functionNavHistory = [];
+  let functionNavHistoryIndex = -1;
 
   // Create a screen object.
   const screen = blessed.screen({
@@ -122,25 +134,93 @@ const interactive = async (chunks) => {
       outputBox.log(`${chalk.bgRed("%")} ${text}`);
     }
 
-    if (text === "exit") {
+    if (text === "") {
+      lastCommandStatus = true;
+    } else if (text === "exit") {
       return process.exit(0);
     } else if (text === "help") {
       for (const [key, value] of Object.entries(helpMenu)) {
         outputBox.log(chalk.cyan(`- '${key}': ${value}`));
       }
       lastCommandStatus = true;
-    } else if (text === "fetch") {
-      outputBox.log(fetchMenu(chunks));
-      lastCommandStatus = true;
-    } else if (text.startsWith("func")) {
-      if (text.split(" ").length !== 2) {
-        outputBox.log(chalk.magenta("Usage: func <function_name>"));
+    } else if (text.startsWith("list")) {
+      if (text.split(" ").length < 2) {
+        outputBox.log(
+          chalk.magenta(
+            "Usage: list <options>\nlist fetch: List functions that contain fetch instances\nlist all: List all functions"
+          )
+        );
+        lastCommandStatus = false;
+      } else {
+        const option = text.split(" ")[1];
+        if (option === "fetch") {
+          outputBox.log(commandHelpers.fetchMenu(chunks));
+          lastCommandStatus = true;
+        } else if (option === "all") {
+          outputBox.log(commandHelpers.listAllFunctions(chunks));
+          lastCommandStatus = true;
+        } else {
+          outputBox.log(chalk.red(option), "is not a valid option");
+          lastCommandStatus = false;
+        }
+      }
+    } else if (text.startsWith("go")) {
+      if (text.split(" ").length < 2) {
+        outputBox.log(chalk.magenta("Usage: go <options>\ngo to <functionID>"));
         lastCommandStatus = false;
       } else {
         const funcName = text.split(" ")[1];
-        const funcCode = getFunctionCode(chunks, funcName);
-        outputBox.log(funcCode);
-        lastCommandStatus = true;
+        if (funcName === "to") {
+          const funcId = text.split(" ")[2];
+          const funcCode = commandHelpers.getFunctionCode(chunks, funcId);
+          outputBox.setText(funcCode);
+          lastCommandStatus = true;
+          functionNavHistory.push(funcId);
+          functionNavHistoryIndex++;
+        } else if (funcName == "back") {
+          // check if the user has navigated to any function
+          if (functionNavHistory.length > 0) {
+            // check if this is the last checked function
+            if (functionNavHistoryIndex > 0) {
+              functionNavHistoryIndex--;
+              const funcId = functionNavHistory[functionNavHistoryIndex];
+              const funcCode = commandHelpers.getFunctionCode(chunks, funcId);
+              outputBox.setText(funcCode);
+              lastCommandStatus = true;
+            } else {
+              // user is already at the first function
+              outputBox.log(chalk.red("No previous function found"));
+              lastCommandStatus = false;
+            }
+          } else {
+            // user has not navigated to any function
+            outputBox.log(chalk.red("No previous function found"));
+            lastCommandStatus = false;
+          }
+        } else if (funcName == "ahead") {
+          // check if the user has navigated to any function
+          if (functionNavHistory.length > 0) {
+            // check if this is the last checked function
+            if (functionNavHistoryIndex < functionNavHistory.length - 1) {
+              functionNavHistoryIndex++;
+              const funcId = functionNavHistory[functionNavHistoryIndex];
+              const funcCode = commandHelpers.getFunctionCode(chunks, funcId);
+              outputBox.setText(funcCode);
+              lastCommandStatus = true;
+            } else {
+              // user is already at the last function
+              outputBox.log(chalk.red("No next function found"));
+              lastCommandStatus = false;
+            }
+          } else {
+            // user has not navigated to any function
+            outputBox.log(chalk.red("No next function found"));
+            lastCommandStatus = false;
+          }
+        } else {
+          outputBox.log(chalk.red(funcName), "is not a valid option");
+          lastCommandStatus = false;
+        }
       }
     } else if (text === "clear") {
       outputBox.setText("");
@@ -158,7 +238,7 @@ const interactive = async (chunks) => {
   inputBox.focus();
 
   // Quit on Escape
-  screen.key(["escape"], () => {
+  inputBox.key(["escape"], () => {
     return process.exit(0);
   });
 
