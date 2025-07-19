e843717420efd2b099b374bda0e0554ddfef6ea5
Shriyans Sudhi <shriyanss@ss0x00.com>
2025-07-19 14:00:28 +0530

    feat: resolve URL and method for axios call and refactor the code

--- src/map/next_js/resolveFetch.ts
+++ src/map/next_js/resolveFetch.ts
@@ -1,4 +1,5 @@
 import chalk from "chalk";
+import { resolveNodeValue } from "./utils.js";
 import parser from "@babel/parser";
 import _traverse from "@babel/traverse";
 import fs from "fs";
@@ -6,114 +7,7 @@ import path from "path";
 import { Chunks } from "../../utility/interfaces.js";
 const traverse = _traverse.default;
 
-const resolveNodeValue = (node, scope) => {
-    if (!node) return null;
 
-    switch (node.type) {
-        case "StringLiteral":
-        case "NumericLiteral":
-        case "BooleanLiteral":
-            return node.value;
-        case "NullLiteral":
-            return null;
-        case "TemplateLiteral":
-            let result = "";
-            for (let i = 0; i < node.quasis.length; i++) {
-                result += node.quasis[i].value.raw;
-                if (i < node.expressions.length) {
-                    result += resolveNodeValue(node.expressions[i], scope);
-                }
-            }
-            return result;
-        case "Identifier": {
-            const binding = scope.getBinding(node.name);
-            if (binding && binding.path.node.init) {
-                return resolveNodeValue(binding.path.node.init, scope);
-            }
-            return `[unresolved: ${node.name}]`;
-        }
-        case "ObjectExpression": {
-            const obj = {};
-            for (const prop of node.properties) {
-                if (prop.type === "ObjectProperty") {
-                    const key = prop.computed
-                        ? resolveNodeValue(prop.key, scope)
-                        : prop.key.name || prop.key.value;
-                    const value = resolveNodeValue(prop.value, scope);
-                    obj[key] = value;
-                } else if (prop.type === "SpreadElement") {
-                    const spreadObj = resolveNodeValue(prop.argument, scope);
-                    if (typeof spreadObj === "object" && spreadObj !== null) {
-                        Object.assign(obj, spreadObj);
-                    }
-                }
-            }
-            return obj;
-        }
-        case "MemberExpression": {
-            const object = resolveNodeValue(node.object, scope);
-            if (typeof object === "object" && object !== null) {
-                const propertyName = node.computed
-                    ? resolveNodeValue(node.property, scope)
-                    : node.property.name;
-                return object[propertyName];
-            }
-            return `[unresolved member expression]`;
-        }
-        case "CallExpression": {
-            if (
-                node.callee.type === "MemberExpression" &&
-                node.callee.property.name === "toString"
-            ) {
-                return resolveNodeValue(node.callee.object, scope);
-            }
-            return `[unresolved call to ${node.callee.name || "function"}]`;
-        }
-        case "NewExpression": {
-            if (
-                node.callee.type === "Identifier" &&
-                node.callee.name === "URL" &&
-                node.arguments.length > 0
-            ) {
-                return resolveNodeValue(node.arguments[0], scope);
-            }
-            return `[unresolved new expression]`;
-        }
-        case "LogicalExpression": {
-            const left = resolveNodeValue(node.left, scope);
-            if (left && !String(left).startsWith("[")) {
-                return left;
-            }
-            return resolveNodeValue(node.right, scope);
-        }
-        case "ConditionalExpression": {
-            const consequent = resolveNodeValue(node.consequent, scope);
-            if (consequent && !String(consequent).startsWith("[")) {
-                return consequent;
-            }
-            return resolveNodeValue(node.alternate, scope);
-        }
-        case "BinaryExpression": {
-            const left = resolveNodeValue(node.left, scope);
-            const right = resolveNodeValue(node.right, scope);
-            if (
-                left !== null &&
-                right !== null &&
-                !String(left).startsWith("[") &&
-                !String(right).startsWith("[")
-            ) {
-                // eslint-disable-next-line default-case
-                switch (node.operator) {
-                    case "+":
-                        return left + right;
-                }
-            }
-            return `[unresolved binary expression: ${node.operator}]`;
-        }
-        default:
-            return `[unsupported node type: ${node.type}]`;
-    }
-};
 
 const resolveFetch = async (chunks: Chunks, directory: string) => {
     console.log(chalk.cyan("[i] Resolving fetch instances"));
