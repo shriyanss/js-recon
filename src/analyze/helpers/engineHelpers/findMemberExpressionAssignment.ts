8beaabdf83078e11fef7f957dc624bdf2c389884
Shriyans Sudhi <shriyanss@ss0x00.com>
2025-07-31 14:16:41 +0530

    refactor: move the function to find member expression assignment to a new file

---
+++ src/analyze/helpers/engineHelpers/findMemberExpressionAssignment.ts
@@ -0,0 +1,35 @@
+import { Node } from "@babel/types";
+import _traverse from "@babel/traverse";
+const traverse = _traverse.default;
+import _generator from "@babel/generator";
+const generator = _generator.default;
+import { highlight } from "cli-highlight";
+
+/**
+ * Traverses a given AST node to find and log assignments between two member expressions.
+ * @param node The AST node to traverse.
+ * @param toMatch The name of the property to match on the left side of the assignment.
+ */
+export const findMemberExpressionAssignment = (node: Node, toMatch: string) => {
+    traverse(node, {
+        AssignmentExpression(path) {
+            const assignmentNode = path.node;
+
+            if (
+                assignmentNode.left?.type === "MemberExpression" &&
+                assignmentNode.right?.type === "MemberExpression" &&
+                (assignmentNode.left as any).property?.type === "Identifier" &&
+                (assignmentNode.left as any).property?.name === toMatch
+            ) {
+                const { code } = generator(assignmentNode);
+                console.log(
+                    highlight(code, {
+                        language: "javascript",
+                        ignoreIllegals: true,
+                        theme: undefined,
+                    })
+                );
+            }
+        },
+    });
+};
