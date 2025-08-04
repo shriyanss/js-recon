df0f64e093e595bcc87e4a3f05dfaa8bcab62816
Shriyans Sudhi <shriyanss@ss0x00.com>
2025-08-04 13:00:43 +0530

    feat: add database population for mapped JSON data in report module

--- src/report/index.ts
+++ src/report/index.ts
@@ -1,11 +1,25 @@
 import chalk from "chalk";
 import initReportDb from "./utility/initReportDb.js";
+import fs from "fs";
+import { Chunks } from "../utility/interfaces.js";
+import { populateMappedJson } from "./utility/populateDb/populateMappedJson.js";
+import Database from "better-sqlite3";
 
 const report = async (sqliteDbPath: string, mappedJsonFilePath: string | undefined) => {
     console.log(chalk.cyan("[i] Running 'report' module"));
 
-    await initReportDb(sqliteDbPath);
-    console.log(chalk.green("[✓] Report database initialized successfully"));
+    // check if db exists. if not, init
+    if (!fs.existsSync(sqliteDbPath)) {
+        await initReportDb(sqliteDbPath);
+        console.log(chalk.green("[✓] Report database initialized successfully"));
+    }
+
+    const db = new Database(sqliteDbPath);
+
+    if (mappedJsonFilePath) {
+        const chunks: Chunks = JSON.parse(fs.readFileSync(mappedJsonFilePath, "utf8"));
+        await populateMappedJson(db, chunks);
+    }
 };
 
 export default report;
