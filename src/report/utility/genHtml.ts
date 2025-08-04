93f7959ade7601dba8b3793eb584f1e685cbfef3
Shriyans Sudhi <shriyanss@ss0x00.com>
2025-08-04 15:43:05 +0530

    feat: add HTML report generation with markdown support (structure)

---
+++ src/report/utility/genHtml.ts
@@ -0,0 +1,36 @@
+import chalk from "chalk";
+import fs from "fs";
+import { marked } from "marked";
+import Database from "better-sqlite3";
+
+const html = async (markdown: string) => {
+    return `<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Markdown Report</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/highlight.js@11.9.0/styles/github-dark.css">
+</head>
+<body>
+  ${await marked(markdown)}
+</body>
+</html>`;
+};
+
+const genHtml = async (outputReportFile: string, db: Database.Database) => {
+    console.log(chalk.cyan("[i] Generating HTML report..."));
+
+    let markdown = `# JS Recon Report generated at ${new Date().toISOString()}\n\n`;
+
+    marked.use({
+        async: true,
+        pedantic: false,
+        gfm: true,
+    });
+    const renderedHtml = await html(markdown);
+    fs.writeFileSync(outputReportFile, renderedHtml);
+
+    console.log(chalk.green("[✓] HTML report generated successfully"));
+};
+
+export default genHtml;
