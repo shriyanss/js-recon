2c36ad05e649517f786adf98013b0ced8e1720d7
Shriyans Sudhi <shriyanss@ss0x00.com>
2025-08-08 11:58:54 +0530

    fix: handle page load timeouts and add error handling for detached frames

--- src/globalConfig.ts
+++ src/globalConfig.ts
@@ -1,6 +1,6 @@
 const githubURL = "https://github.com/shriyanss/js-recon";
 const modulesDocs = "https://js-recon.io/docs/category/modules";
-const version = "1.2.1-beta.1";
+const version = "1.2.1-beta.2";
 const toolDesc = "JS Recon Tool";
 const axiosNonHttpMethods = ["isAxiosError"]; // methods available in axios, which are not for making HTTP requests
 
