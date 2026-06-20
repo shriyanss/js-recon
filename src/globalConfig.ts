0624eb42876c6c32b913360ac019b2e9e9875b19
Shriyans Sudhi <shriyanss@ss0x00.com>
2026-06-20 21:41:31 +0530

    chore: bump version to 1.4.1-alpha.2 and update CHANGELOG

--- src/globalConfig.ts
+++ src/globalConfig.ts
@@ -1,6 +1,6 @@
 const githubURL = "https://github.com/shriyanss/js-recon";
 const modulesDocs = "https://js-recon.io/docs/category/modules";
-const version = "1.4.1-alpha.1";
+const version = "1.4.1-alpha.2";
 const toolDesc =
     "Static analysis tool that maps API endpoints and detects client-side security issues by analyzing webpack/turbopack bundles";
 const axiosNonHttpMethods = ["isAxiosError"]; // methods available in axios, which are not for making HTTP requests
